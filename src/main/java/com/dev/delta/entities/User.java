6e679bc4fc4967bfbeb0204631e6c1b117092726
Anis KCHAOU <16862158+aniskchaou@users.noreply.github.com>
2023-09-24 12:24:48 +0200

    Update User.java

--- src/main/java/com/dev/delta/entities/User.java
+++ src/main/java/com/dev/delta/entities/User.java
@@ -5,7 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
+@Table(name = "userrrr")
 @Entity
 public class User {
 	@Id
